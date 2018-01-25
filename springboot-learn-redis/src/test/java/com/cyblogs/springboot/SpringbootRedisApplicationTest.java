3ef3d3e9985f61f0d57ec7c13b14cb2c30b3704b
chenyuan <chengcheng222e@sina.com>
2018-01-25 20:16:30 +0800

    连接redis

---
+++ springboot-learn-redis/src/test/java/com/cyblogs/springboot/SpringbootRedisApplicationTest.java
@@ -0,0 +1,26 @@
+package com.cyblogs.springboot;
+
+import com.cyblogs.springboot.dao.RedisDao;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class SpringbootRedisApplicationTest {
+
+    @Autowired
+    private RedisDao redisDao;
+
+    @Test
+    public void testRedis() {
+        redisDao.setKey("name", "chenyuan");
+        redisDao.setKey("age", "11");
+        log.info(redisDao.getValue("name"));
+        log.info(redisDao.getValue("age"));
+    }
+}
